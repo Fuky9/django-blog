ed034e9ead750aaa0e70a01335561c44ac164578
Fuky9 <lukasfukan6@gmail.com>
2024-03-06 20:59:41 +0100

    Added base template and basic styling CSS framework + settings to Templates DIRS to make django-blog/templates directory work

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'personal_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates/" # added to make Django-blog/Templates folder functional
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
